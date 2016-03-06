ba2ab4a5db870c43a1b68e8bf81add07aabb2340
AriFlo <ariflo@arianflores.com>
2016-03-05 23:14:15 -0800

    set-up contruction image

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,14 @@ var ariApp = angular.module('ariApp', ['ngRoute', 'ngAnimate', 'ngResource']);
 ariApp.config(function($routeProvider, $locationProvider,$httpProvider){
 	$routeProvider
 	.when('/',{
-		templateUrl: 'views/homepage.html',
+		templateUrl: 'views/comingSoon.html',
         		controller: 'homeController'
-	})		
+	})	
+
+	$locationProvider.html5Mode( {
+  		enabled: true,
+  		requireBase: false
+	});
+
+
 });
