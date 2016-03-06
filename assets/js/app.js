82e726423ed8b1851b6e3fcc9917095f39c2a676
AriFlo <ariflo@arianflores.com>
2016-03-05 23:22:48 -0800

    testing git push

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ var ariApp = angular.module('ariApp', ['ngRoute', 'ngAnimate', 'ngResource']);
 ariApp.config(function($routeProvider, $locationProvider,$httpProvider){
 	$routeProvider
 	.when('/',{
-		templateUrl: 'views/comingSoon.html',
+		templateUrl: 'views/homepage.html',
         		controller: 'homeController'
 	})	
 
