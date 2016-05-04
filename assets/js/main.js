e084f99f2f6d0486a969c7700973e111eea38abf
AriFlo <ariflo@arianflores.com>
2016-05-03 21:18:56 -0700

    finshed styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 // add scripts
 
+$('head').append('<style>#typed-output {color: #E1D463} #fuckerSemicolon {color:#FF4053;}</style>');
+
 $(document).on('ready', function() {
-	console.log("sanity check!");
+	var semiHere = false;
+	   var valr = setInterval(function(){
+	       var currTxt = $('#typed-output').html();
+	       semiHere = ~currTxt.indexOf(';');
+	       if (semiHere) {
+	           if (~currTxt.indexOf('"Software Engineer"<span')) clearInterval(valr);
+	           if (!~currTxt.indexOf('span')) {
+	               var newHTML = $('#typed-output').html().replace(/;/, '<span id="fuckerSemicolon">;</span>');
+	               $('#typed-output').html(newHTML);
+	           }
+	       }
+	   }, 20);
 });
