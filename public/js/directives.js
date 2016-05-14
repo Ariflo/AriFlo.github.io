325de1c3b1d771557f2e634a56db757136fb24a8
AriFlo <ariflo@arianflores.com>
2016-05-14 12:17:42 -0700

    added all projects cleaning bugs

--- public/js/directives.js
+++ public/js/directives.js
@@ -25,7 +25,7 @@ ariApp.directive('modal', function () {
             '<div class="modal-content">' + 
               '<div class="modal-header">' + 
                 '<button type="button" class="close" data-dismiss="modal" ng-click="return()" aria-hidden="true">&times;</button>' + 
-                '<h4 class="modal-title">{{ title }}</h4>' +
+                '<h4 class="modal-title">{{projectTitle}}</h4>' +
               '</div>' + 
               '<div class="modal-body" ng-transclude></div>' + 
             '</div>' + 
